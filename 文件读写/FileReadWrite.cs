a0b82bc7ee3cbb3c3277fdf3c277ce9dbfe44644
TangZe <76731956+Chai-TangZe@users.noreply.github.com>
2021-01-11 16:25:18 +0800

    增加StreamingAssets目录文件读写

--- 文件读写/FileReadWrite.cs
+++ 文件读写/FileReadWrite.cs
@@ -8,12 +8,13 @@ public class FileReadWrite : MonoBehaviour
     static string Error = "错误！没有寻找到文件，建议查看文件名称是否输入错误？";
 
     static List<string> datas = new List<string>();
+
     /// <summary>
-    /// 读取全部文件
+    /// 在本地读取文件全部数据
     /// </summary>
     /// <param name="txtName">文件名</param>
     /// <returns>返回每行</returns>
-    public static List<string> ReadData(string txtName)
+    public static List<string> ReadLocalData(string txtName)
     {
         datas.Clear();
         StreamReader sr = null;//读取
@@ -39,11 +40,11 @@ public class FileReadWrite : MonoBehaviour
     }
 
     /// <summary>
-    /// 写入一行文件
+    /// 在本地写入一行数据
     /// </summary>
     /// <param name="txtName">文件名</param>
     /// <param name="data">写入数据</param>
-    public static void WriteTxt(string txtName, string data)
+    public static void WriteLocalTxt(string txtName, string data)
     {
         StreamWriter sw;//写入
         FileInfo t = new FileInfo(Application.persistentDataPath + "\\" + txtName);//文件流 
@@ -62,25 +63,25 @@ public class FileReadWrite : MonoBehaviour
     }
 
     /// <summary>
-    /// 删除文件
+    /// 删除本地文件
     /// </summary>
     /// <param name="txtName"></param>
-    public static void DeleteTxt(string txtName)
+    public static void DeleteLocalTxt(string txtName)
     {
         FileInfo t = new FileInfo(Application.persistentDataPath + "\\" + txtName);//文件流 
         t.Delete();
     }
 
     /// <summary>
-    /// 复制文件内容
+    /// 复制本地文件的全部内容
     /// </summary>
     /// <param name="txtName"></param>
     /// <returns></returns>
-    public static bool CopyTxt(string txtName)
+    public static bool CopyLocalTxt(string txtName)
     {
         bool IsCopyTxt = false;
         string CopyContent = "";
-        ReadData(txtName);
+        ReadLocalData(txtName);
         foreach (string data in datas)
         {
             CopyContent += data + "\n";
@@ -96,4 +97,57 @@ public class FileReadWrite : MonoBehaviour
         GUIUtility.systemCopyBuffer = CopyContent;
         return IsCopyTxt;
     }
+
+    /// <summary>
+    /// 读取StreamingAssets目录文件
+    /// </summary>
+    /// <param name="path">路径："/文件名加后缀"</param>
+    /// <returns>返回字符串</returns>
+    public static List<string> ReadStreamingAssetsData(string path)
+    {
+        string Path = GetStreamingAssetsPath();
+
+        path = Path + path;
+
+        //打开文件
+        StreamReader tmpReader = new StreamReader(path);
+        datas.Clear();
+        string ReadRow = tmpReader.ReadLine();
+        while (ReadRow != null && ReadRow != "")
+        {
+            datas.Add(ReadRow);
+            ReadRow = tmpReader.ReadLine();
+        }
+        tmpReader.Close();
+        return datas;
+    }
+
+    /// <summary>
+    /// StreamingAssets目录写入一行数据
+    /// </summary>
+    /// <param name="path"></param>
+    public static void WriteStreamingAssetsData(string path, string data)
+    {
+        ReadStreamingAssetsData(path);
+        string Path = GetStreamingAssetsPath();
+        path = Path + path;
+        StreamWriter tmpWrite = new StreamWriter(path);
+        foreach (string linedata in datas)
+        {
+            tmpWrite.WriteLine(linedata);
+        }
+        tmpWrite.WriteLine(data);
+        tmpWrite.Close();
+    }
+
+    /// <summary>
+    /// 得到StreamingAssets目录
+    /// </summary>
+    /// <returns></returns>
+    static string GetStreamingAssetsPath()
+    {
+        string str = Application.streamingAssetsPath + "/";
+        return str;
+    }
+
 }
