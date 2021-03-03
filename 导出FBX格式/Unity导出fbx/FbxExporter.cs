e105f3f7d6bff85aa9aa38471b36b48411799080
TangZe <76731956+Chai-TangZe@users.noreply.github.com>
2021-03-03 10:09:44 +0800

    清除重复信息

---
+++ 导出FBX格式/Unity导出fbx/FbxExporter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FbxExporter : MonoBehaviour {
+    private string fbxname = "fbx";
+    public GameObject[] meshObjs;
+    // Use this for initialization
+    void Start () {
+        FBXExporter.ExportFBX("", fbxname, meshObjs, true);
+    }
+}
