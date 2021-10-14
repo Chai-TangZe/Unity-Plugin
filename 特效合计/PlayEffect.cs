97e596f14cfa54137d172f32fd4596cb7a775f37
唐泽 <chaiyuan9826@gmail.com>
2021-10-14 15:54:05 +0800

    更新

---
+++ 特效合计/PlayEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEffect : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+    private void OnEnable()
+    {
+        if (!particleSystem)
+            particleSystem = GetComponent<ParticleSystem> ();
+        particleSystem.Play ();
+    }
+    private void OnDisable()
+    {
+        if (!particleSystem)
+            particleSystem = GetComponent<ParticleSystem> ();
+        particleSystem.Stop ();
+    }
+}
